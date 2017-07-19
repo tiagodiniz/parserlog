89bedda78de5e1589a1ac7ad92c76096bd97e4af
Tiago <tiago.diniz@gmail.com>
2017-07-18 21:16:03 -0300

    implmentation echo framework restfull

--- Main.go
+++ Main.go
@@ -1,1 +1,27 @@
-package parserlog
+package main
+
+import (
+	"github.com/labstack/echo"
+	"github.com/labstack/echo/middleware"
+)
+
+func main() {
+
+	// new echo instance
+	e := echo.New()
+
+	// set debug mode
+	e.Debug = true
+
+	// set middleware
+	e.Use(middleware.Logger())
+	e.Use(middleware.Recover())
+
+	// add static paths
+	e.Static("/", "static")
+
+
+	// start server
+	e.Start(":8888")
+
+}
