1812e14e9cf6274772f5c169fbd6a7e6406b15ed
Tiago <tiago.diniz@gmail.com>
2017-07-18 21:42:26 -0300

    create regex on parser nodes logs

---
+++ util/rules.go
@@ -0,0 +1,15 @@
+package etl
+
+// used regexp2 instead regexp for better performance
+import (
+	"github.com/dlclark/regexp2"
+)
+
+// rules
+var kill, killer, killed *regexp2.Regexp
+
+func init() {
+	kill = regexp2.MustCompile(`:\s([^:]+)\skilled\s(.*?)\sby\s[a-zA-Z_]+`, 0)
+	killer = regexp2.MustCompile(`(?<=\d:\s)(.*?)(?=\skilled)`, 0)
+	killed = regexp2.MustCompile(`(?<=killed\s)(.*?)(?=\sby)`, 0)
+}
