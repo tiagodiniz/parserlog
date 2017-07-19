e60b9afcf750f20e9be5e9cce957aa75612009cb
Tiago <tiago.diniz@gmail.com>
2017-07-18 22:00:41 -0300

    implement rest api

--- api/api.go
+++ api/api.go
@@ -1,2 +1,23 @@
 package api
 
+import (
+	"github.com/labstack/echo"
+	"net/http"
+	"github.com/parserlog/parser"
+)
+
+func API(ctx echo.Context) error {
+
+	// parser log files
+
+	result, err := parser.Process()
+
+	if err == nil {
+
+		// render response
+		err = ctx.JSON(http.StatusOK, result)
+
+	}
+
+	return err
+}
