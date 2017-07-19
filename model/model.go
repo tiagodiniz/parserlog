1176d8d5ff921b4a47f5dfd44c3350516a710ed0
Tiago <tiago.diniz@gmail.com>
2017-07-18 21:35:54 -0300

    create model

--- model/model.go
+++ model/model.go
@@ -1,1 +1,10 @@
 package model
+
+type BodyCount struct {
+	TotalKills int            `json:"total_kills"`
+	Players    []string       `json:"players"`
+	Kills      map[string]int `json:"kills"`
+	FilePath   string         `json:"-"`
+	Error      error          `json:"-"`
+}
+
